7da9b7e3551e515e7a4c2e0f2e979d6cd399bf16
mrdcs92 <mrdcs92@gmail.com>
2017-09-05 21:13:41 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function randomQuote() {
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/?",
+    dataType: "jsonp",
+    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function(response) {
+      $("#random_quote").html("<p id='random_quote' class='lead text-center'>" + response.quoteText + "<br/>&dash; " + response.quoteAuthor + "</p>");
+    }
+  })
+}
+
+$(function() {
+  randomQuote();
+});
+
+$("button").click(function(){
+  randomQuote();
+});
